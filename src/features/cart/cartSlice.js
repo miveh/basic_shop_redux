bfcf8392a4a286603b8d45e46603ec01cf93ed58
miveh <miveh@imivechi@gmail.com>
2024-01-17 19:36:38 +0330

    cartslice and actions added

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -8,14 +8,35 @@ const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    increment(state) {
-      state.value++;
+    increment: (state, action) => {
+      const index = state.cartItems.findIndex(
+        (item) => item.product.id === action.payload.id
+      );
+      state.cartItems[index].count += 1;
     },
-    decrement(state) {
-      state.value--;
+    decrement: (state, action) => {
+      const index = state.cartItems.findIndex(
+        (item) => item.product.id === action.payload.id
+      );
+      const count = state.cartItems[index].count;
+      if (count === 1) {
+        state.cartItems = state.cartItems.filter(
+          (item) => item.product.id === action.payload.id
+        );
+      } else {
+        state.cartItems[index].count -= 1;
+      }
     },
-    incrementByAmount(state, action) {
-      state.value += action.payload;
+    addProduct: (state, action) => {
+      state.cartItems.push({ product: action.payload, count: 1 });
+    },
+    removeProduct: (state, action) => {
+      const index = state.cartItems.filter(
+        (item) => item.product.id === action.payload.id
+      );
+    },
+    clearCart: (state) => {
+      state.cartItems = [];
     },
   },
 });
